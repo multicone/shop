8432dc20947b48250260c5ee44f8010ff409988f
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2021-07-12 12:46:44 +0600

    Made responsive and added AOS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const [show, setShow] = useState(false)
   return (
     <nav className="bg-gray-100 w-full flex items-center py-4">
-      <div className="container mx-auto flex flex-col md:flex-row md:justify-between md:items-center">
+      <div className="container mx-auto px-4 lg:px-16 flex flex-col md:flex-row md:justify-between md:items-center">
         <div className="flex justify-between items-center">
           <p className="text-3xl text-gray-700 font-semibold">
             Multi<span className="text-green-500">cone</span>
@@ -24,24 +24,24 @@ const Header = () => {
         </div>
 
         <div
-          className={`px-3 flex-col md:flex-row justify-center items-center bg-gray-100 md:flex transition-all duration-500 ease-in ${
+          className={`flex-col md:flex-row justify-center items-center md:flex transition-all duration-500 ease-in ${
             show ? 'flex' : 'hidden'
           }`}
         >
           <a
-            href="#"
+            href="/home"
             className="md:px-2 py-2 text-gray-700 hover:text-green-500"
           >
             Home
           </a>
           <a
-            href="#"
+            href="/products"
             className="md:px-2 py-2 text-gray-700 hover:text-green-500"
           >
             Products
           </a>
           <a
-            href="#"
+            href="/about"
             className="md:px-2 mb-2 md:mb-0 py-2  text-gray-700 hover:text-green-500"
           >
             About
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,20 +2,32 @@ import React from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
 import HeroSvg from '../assets/images/hero.svg'
 import '../assets/css/hero-svg.css'
+import aos from 'aos'
+import 'aos/dist/aos.css'
+
+aos.init({
+  duration: 1000,
+  easing: 'ease',
+})
 
 const HeroSection = () => {
   return (
-    <div className="hero-sec h-screen w-full py-8">
-      <div className="container mx-auto flex items-center justify-between py-16">
-        <div className="w-1/2">
-          <h1 className="text-gray-700 md:text-5xl text-3xl font-bold py-5">
+    <div className="hero-sec h-screen w-full py-4">
+      <div className="container mx-auto flex items-center flex-col md:flex-row md:items-start md:justify-between px-4 py-8 lg:py-16 lg:px-16">
+        <div
+          data-aos="fade-right"
+          className="flex flex-col items-center md:items-start lg:mt-16 md:mt-8"
+        >
+          <h1 className="text-gray-700 lg:text-5xl text-3xl font-bold py-5 text-center md:text-left">
             Groceries Delivered in 90 Minute
           </h1>
-          <p className="text-gray-700 text-lg py-1">
+
+          <p className="text-gray-700 text-md lg:text-lg py-1 text-center md:text-left">
             Get your healthy foods & snacks delivered at your doorsteps all day
             everyday
           </p>
-          <button className="btn-primary my-8 flex items-center">
+
+          <button className="btn-primary block my-8 flex items-center justify-self-center md:justify-self-start">
             <span className="mr-2">
               <FaShoppingCart />
             </span>
@@ -23,8 +35,8 @@ const HeroSection = () => {
           </button>
         </div>
         {/* SVG */}
-        <div className="">
-          <img src={HeroSvg} className="hero-svg" />
+        <div data-aos="fade-left">
+          <img src={HeroSvg} alt="hero-svg" className="hero-svg" />
         </div>
       </div>
     </div>
